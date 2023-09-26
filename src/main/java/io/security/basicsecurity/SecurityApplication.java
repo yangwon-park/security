03388928c02e13f091d0c3230c553aa004ac97ca
Yangwon Park <ywonp94@gmail.com>
2023-09-26 11:13:11 +0900

    *sparkles* Project Init

---
+++ src/main/java/io/security/basicsecurity/SecurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
