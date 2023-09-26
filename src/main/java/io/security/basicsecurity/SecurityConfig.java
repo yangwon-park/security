2db6248b8377c6302c6eeab02572df3584a55d4c
Yangwon Park <ywonp94@gmail.com>
2023-09-26 12:03:44 +0900

    *sparkles* Feature - formLogin 인증 방식 구현.

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,37 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth -> auth
+                        .anyRequest().authenticated()))
+                .formLogin((formLogin) ->
+                        formLogin
+//                                .loginPage("/loginPage")
+                                .defaultSuccessUrl("/")
+                                .failureUrl("/login")
+                                .usernameParameter("userId")
+                                .passwordParameter("passwd")
+                                .loginProcessingUrl("/login_proc")
+                                .successHandler((request, response, authentication) -> {
+                                    System.out.println("authentication " + authentication.getName());
+                                    response.sendRedirect("/");
+                                })
+                                .failureHandler((request, response, exception) -> {
+                                    System.out.println("exception " + exception.getMessage());
+                                    response.sendRedirect("/login");
+                                })
+                                .permitAll());
+
+        return http.build();
+    }
+}
