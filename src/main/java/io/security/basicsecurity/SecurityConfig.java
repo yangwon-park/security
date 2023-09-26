59ff100d0a2eebe4ecb570f79cd10e8166cd4fde
Yangwon Park <ywonp94@gmail.com>
2023-09-26 15:11:17 +0900

    *sparkles* Feature - remember me 인증 필터 구현.

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,14 +1,20 @@
 package io.security.basicsecurity;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,7 +36,25 @@ public class SecurityConfig {
                                     System.out.println("exception " + exception.getMessage());
                                     response.sendRedirect("/login");
                                 })
-                                .permitAll());
+                                .permitAll())
+                .logout((logout) ->
+                        logout.logoutUrl("/logout")
+                                .logoutSuccessUrl("/login")
+                                .addLogoutHandler(((request, response, authentication) -> {
+                                    HttpSession session = request.getSession();
+                                    session.invalidate();
+                                })).logoutSuccessHandler((request, response, authentication) -> {
+                                    System.out.println("logout succeed");
+                                    response.sendRedirect("/login");
+                                })
+                                .deleteCookies("remember-me"))
+
+                .rememberMe((remember) -> {
+                    remember.rememberMeParameter("remember")
+                            .tokenValiditySeconds(3600) // default: 14일
+                            .alwaysRemember(false)
+                            .userDetailsService(userDetailsService);
+                });
 
         return http.build();
     }
