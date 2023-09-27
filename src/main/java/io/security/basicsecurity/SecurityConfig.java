383fe2a7dfcc4dc29fcd1885409b0b943c2b286e
Yangwon Park <ywonp94@gmail.com>
2023-09-27 14:55:12 +0900

    *sparkles* Feature - sessionManagement 추가.

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,7 +19,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth -> auth
+        http
+                .authorizeHttpRequests((auth -> auth
                         .anyRequest().authenticated()))
                 .formLogin((formLogin) ->
                         formLogin
@@ -38,7 +40,8 @@ public class SecurityConfig {
                                 })
                                 .permitAll())
                 .logout((logout) ->
-                        logout.logoutUrl("/logout")
+                        logout
+                                .logoutUrl("/logout")
                                 .logoutSuccessUrl("/login")
                                 .addLogoutHandler(((request, response, authentication) -> {
                                     HttpSession session = request.getSession();
@@ -50,10 +53,19 @@ public class SecurityConfig {
                                 .deleteCookies("remember-me"))
 
                 .rememberMe((remember) -> {
-                    remember.rememberMeParameter("remember")
+                    remember
+                            .rememberMeParameter("remember")
                             .tokenValiditySeconds(3600) // default: 14일
                             .alwaysRemember(false)
                             .userDetailsService(userDetailsService);
+                })
+                .sessionManagement((sm) -> {
+                    sm
+                            .sessionFixation().changeSessionId() // default
+                            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // default: 필요 시 세션 생성 (JWT 쓰는 경우엔 Stateless 정책 사용)
+//                            .maximumSessions(1)
+//                            .maxSessionsPreventsLogin(false)    // true: 세션이 초과되면 아예 로그인 못 함
+                    ;
                 });
 
         return http.build();
